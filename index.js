a1b181e278b1cb96d65d139b1a0a343253fa290e
vetrivel44-bit <vel21873@gmail.com>
2026-05-15 20:39:25 +0530

    Fix dotenv path in index.js to load backend/.env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require("path");
-require("dotenv").config();
+require("dotenv").config({ path: path.join(__dirname, "backend", ".env") });
 
 const app = require("./backend/src/app");
 const { config } = require("./backend/src/config/env");
