6bfeae3cea5707e1b8be76cc284b5cf1ba0b08ae
vetrivel44-bit <vel21873@gmail.com>
2026-08-29 11:41:24 +0530

    Mount Voice Cover inside VetroAI application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import VoiceCoverLauncher from './components/screens/VoiceCoverLauncher.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <VoiceCoverLauncher />
   </StrictMode>,
 )
